02c3fbdf8f1b8be50af52093f5dc8f7d541dd2dd
Gaston095 <Gastonsalas095@gmail.com>
2024-03-19 19:34:44 -0300

    Se agregan estilos al about

--- portfolio-gaston/src/Components/About/About.jsx
+++ portfolio-gaston/src/Components/About/About.jsx
@@ -1,19 +1,20 @@
 import styles from "./About.module.css";
-
+import svgPc from "../../assets/svgAboutPc.svg";
 
 export default function About() {
   return (
     <>
       <div className={styles.container}>
-        <div>
-          <label>Hola, mi nombre es</label>
-          <h1 className={styles.title}>Gaston Salas</h1>
-          <label>Soy Frontend Development</label>
+        <div className={styles.containerTxt}>
+          <label className={styles.labelGreeting}>Hola, mi nombre es</label>
+          <h1 className={styles.titleName}>Gaston Salas</h1>
+          <label className={styles.titleProfession}>Soy Frontend Development</label>
           <p>Algo sobre mi</p>
         </div>
-      </div>
-      <div>
-        <img/>
+
+        <div>
+          <img className={styles.imgAbout} src={svgPc} />
+        </div>
       </div>
     </>
   );
