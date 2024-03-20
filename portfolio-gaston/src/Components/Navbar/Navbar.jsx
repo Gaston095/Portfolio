82e2a0382a9fee76ad14f3628c46ad39e07380f9
Gaston095 <Gastonsalas095@gmail.com>
2024-03-20 17:31:31 -0300

    Se agrega menu sideBar

--- portfolio-gaston/src/Components/Navbar/Navbar.jsx
+++ portfolio-gaston/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
 import styles from "../Navbar/Navbar.module.css";
+import Sidebar from "../Sidebar/Sidebar";
+import { useState } from "react";
+
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
   return (
     <>
       <div className={styles.navbar}>
-        <h1>Soy el navbar</h1>
+        <h1>Gaston Salas</h1>
+
+        <button onClick={()=> setOpen(true)} className={styles.btnNavbar}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+
+        {open && <Sidebar open={open} setOpen={setOpen}/>}
       </div>
     </>
   );
