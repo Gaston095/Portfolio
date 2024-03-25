f6a9c6706cbfc1492e992d2e90f4f4fff9ba58bb
Gaston095 <Gastonsalas095@gmail.com>
2024-03-25 11:11:06 -0300

    Se agrega seccion de 'Experiencia'.

---
+++ portfolio-gaston/src/Components/Experience/Experience.jsx
@@ -0,0 +1,9 @@
+import styles from './Experience.module.css'
+
+export default function Experience() {
+    return (
+        <>
+            <h1 className={styles.nameSection}>Experiencia</h1>
+        </>
+    )
+}
