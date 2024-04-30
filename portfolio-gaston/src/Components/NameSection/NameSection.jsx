906ee6b09979464a93a52989edd8cbc7372b36c2
Gaston095 <Gastonsalas095@gmail.com>
2024-04-30 13:03:03 -0300

    Se agrega navegacion entre secciones

--- portfolio-gaston/src/Components/NameSection/NameSection.jsx
+++ portfolio-gaston/src/Components/NameSection/NameSection.jsx
@@ -1,5 +1,5 @@
 import styles from './NameSection.module.css'
 
 export default function NameSection({children}) {
-    return <h1 className={styles.nameSection}>{children}</h1>
+    return <h1 id={children} className={styles.nameSection}>{children}</h1>
 }
