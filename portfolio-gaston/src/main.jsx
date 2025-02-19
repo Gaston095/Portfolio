37b61660b8d22e9973d793f0cc92466f9d813012
Gaston095 <Gastonsalas095@gmail.com>
2025-02-19 00:15:20 -0300

    Se agregan react-router para manejar la vista detalle de los proyectos

--- portfolio-gaston/src/main.jsx
+++ portfolio-gaston/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CarDetail from "./Components/CardDetail/CardDetail.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/project/:id" element={<CarDetail />} />
+    </Routes>
+  </BrowserRouter>
+);
